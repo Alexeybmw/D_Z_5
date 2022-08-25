7528458bc32e4ba772148e53e83ad64065c3373b
Alexeybmw <109792704+Alexeybmw@users.noreply.github.com>
2022-08-25 11:11:45 +0300

    zadacha 34,36,38

---
+++ zadacha_34/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] numbers = new int[10];
+
+static void FillArray (int [] array, int min,int max);
+{
+      for(int i = 0;i < array.Length;i++)
+      {
+      int array[i] = new Random().Next(1,10);
+      }
+}
+void WriteArray(int[]array)
+{
+      for(int i = 0;i < array.Length;i++)
+            {
+             Console.Write(array[i]+ " ");
+            }
+      Console.WriteLine();
+}
+int even (int[]array)
+{
+      int quantity = 0;
+      for(int i = 0;i < array.Length;i++)
+      {
+            if(array[i] % 2 == 1)
+            {
+                  quantity++;
+            }
+      }
+      return quantity;
+}
+
+FillArray(numbers,100,1000);
+WriteArray(numbers);
+Console.WriteLine();
+int quantity = even (numbers);
+Console.WriteLine($"количество четных чисел : {even}");
---
+++ zadacha_38/Program.cs
@@ -0,0 +1,21 @@
+﻿double [] arrayRealNumbers = new double[10];
+      for(int i = 0;i < arrayRealNumbers.Length;i++)
+      {
+            arrayRealNumbers[i] = new Random().Next(1,10);
+            Console.Write(arrayRealNumbers[i]+ " ");
+      }
+double maxNumbers = arrayRealNumbers[0];
+double minNumbers = arrayRealNumbers[0];
+      for(int i = 1;i < arrayRealNumbers.Length;i++)
+      {
+            if(maxNumbers < arrayRealNumbers[i])
+            {
+                  maxNumbers = arrayRealNumbers[i];
+            }
+            if(minNumbers > arrayRealNumbers [i])
+            {
+                  minNumbers = arrayRealNumbers[i];
+            }
+      }
+double decision = maxNumbers - minNumbers;
+Console.WriteLine($"Разница между максимальным ({maxNumbers}) и минимальнымны({minNumbers})элементами : {decision}");
